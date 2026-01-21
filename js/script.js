5f17fc87f414ef6e8a51ab2d0f60d922549ee03e
Rafael Toledo Souza <ch1at@outlook.com>
2026-01-21 14:58:38 -0300

    apply shadow rules

--- js/script.js
+++ js/script.js
@@ -47,6 +47,26 @@ class BoxShadowGenerator {
     this.webkitRule.innerText = this.currentRule;
     this.mozRule.innerText = this.currentRule;
   }
+
+  updateValue(type, value) {
+    switch (type) {
+      case "horizontal":
+        this.horizontalRef.value = value;
+        break;
+      case "vertical":
+        this.verticalRef.value = value;
+        break;
+      case "blur":
+        this.blurRef.value = value;
+        break;
+      case "spread":
+        this.spreadRef.value = value;
+        break;
+    }
+
+    this.applyRule();
+    this.showRule();
+  }
 }
 
 // Element selection
@@ -83,3 +103,26 @@ const boxShadow = new BoxShadowGenerator(
 boxShadow.initialize();
 
 // Events
+horizontal.addEventListener("input", (e) => {
+  const value = e.target.value;
+
+  boxShadow.updateValue("horizontal", value);
+});
+
+vertical.addEventListener("input", (e) => {
+  const value = e.target.value;
+
+  boxShadow.updateValue("vertical", value);
+});
+
+spread.addEventListener("input", (e) => {
+  const value = e.target.value;
+
+  boxShadow.updateValue("spread", value);
+});
+
+blur.addEventListener("input", (e) => {
+  const value = e.target.value;
+
+  boxShadow.updateValue("blur", value);
+});
